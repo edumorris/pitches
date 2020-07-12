7b7151b8d9183fef2608415f3bdf92be71897cc6
edumorris <eduuwachira@gmail.com>
2020-07-12 10:42:37 +0300

    Initial project configuration effected

---
+++ config.py
@@ -0,0 +1,28 @@
+import os
+
+class Config:
+    '''
+    General configparent class
+    '''
+
+    # Email configuration
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    # SimpleMDE Config
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+class ProdConfig(Config):
+    pass
+
+class DevConfig(Config):
+    Debug = True
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig,
+}
