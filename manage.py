f705160794de9b63c197a9092c42c4d962b6737b
edumorris <eduuwachira@gmail.com>
2020-07-12 11:00:02 +0300

    Python shell addition & start.sh file configuration effected

--- manage.py
+++ manage.py
@@ -1,12 +1,25 @@
 from app import create_app, db
 from flask_script import Manager, Server
-# from app.models import
+from app.models import Comment
 from flask_migrate import Migrate, MigrateCommand
 
 # App instance
 app = create_app('test')
 
 manager = Manager(app)
+manager.add_command('server', Server)
+@manager.command
+def test():
+       '''
+       Run the unit test
+       '''
+       import unittest
+       tests = unittest.TestLoader().discover('tests')
+       unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+       return dict(app = app, db = db, Comment = Comment)
 
 migrate = Migrate(app, db)
 migrate.add_command('db', MigrateCommand)
---
+++ start.sh
@@ -0,0 +1,4 @@
+export MAIL_USERNAME=servingmain@gmail.com
+export MAIL_PASSWORD=P0r-he-6565
+
+python3 manage.py server
