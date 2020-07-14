e7f9cc39f9037a3b8b6936ce7773896ef444af19
edumorris <eduuwachira@gmail.com>
2020-07-14 11:01:14 +0300

    User profile view affected

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, abort
 from . import main
-from ..models import Comment
+from ..models import Comment, User, Pitch
 from flask_login import login_required
 
 # Main page
@@ -16,3 +16,13 @@ def index():
 @login_required
 def new_comment(id):
     pass
+
+@main.route('/user/<uname>')
+@login_required
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+    
+    return render_template("profile/profile.html", user = user)
